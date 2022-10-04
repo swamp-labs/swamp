9311e06b2dcc4c42103aba7357cbbc3349daf60e
R-Loic <loic.roose@davidson.fr>
2022-10-04 13:05:35 +0200

    test(templateString): fix codesmells

--- engine/templateString/templateString_test.go
+++ engine/templateString/templateString_test.go
@@ -4,47 +4,38 @@ import (
 	"testing"
 )
 
-func TestTemplateString_ToStringWithOneKey(t *testing.T) {
+func TestTemplateStringToStringWithOneKey(t *testing.T) {
 	ts := TemplateString{Keys: []string{"id"}, Format: "%s"}
 	c := make(map[string]string)
-	c["id"] = "id-value"
+	c["id"] = "value"
 
-	s, err := ts.ToString(c)
+	s, _ := ts.ToString(c)
 
-	if err != nil {
-		t.Error("ToString was incorrect", err)
-	}
 	if s != c["id"] {
-		t.Error("String should be equal to id-value, result:", s)
+		t.Error("String should be equal to value, result:", s)
 	}
 }
 
-func TestTemplateString_ToStringWithTwoKeys(t *testing.T) {
+func TestTemplateStringToStringWithTwoKeys(t *testing.T) {
 	ts := TemplateString{Keys: []string{"id", "token"}, Format: "%s:%s"}
 	c := make(map[string]string)
 	c["id"] = "id-value"
 	c["token"] = "token-value"
 
-	s, err := ts.ToString(c)
+	s, _ := ts.ToString(c)
 
-	if err != nil {
-		t.Error("ToString was incorrect", err)
-	}
 	if s != c["id"]+":"+c["token"] {
 		t.Error("String should be equal to id-value:id-token, result:", s)
 	}
 }
 
-func TestTemplateString_ToStringNoMatchingKey(t *testing.T) {
+func TestTemplateStringToStringNoMatchingKey(t *testing.T) {
 	ts := TemplateString{Keys: []string{"token"}, Format: "%s"}
 	c := make(map[string]string)
-	c["id"] = "id-value"
+	c["id"] = "value"
 
-	s, err := ts.ToString(c)
+	s, _ := ts.ToString(c)
 
-	if err != nil {
-		t.Error("ToString was incorrect", err)
-	}
 	if s != "" {
 		t.Error("String should be empty, result:", s)
 	}
