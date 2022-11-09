844bd814108af2e7db06fdbe2ea36606c8400984
R-Loic <loic.roose@davidson.fr>
2022-11-09 10:25:16 +0100

    feat(engine): update yaml format to tasks

--- engine/volume/volume.go
+++ engine/volume/volume.go
@@ -1,13 +1,3 @@
 package volume
 
-//type Volume interface {
-//	//GetRequestGroup() string
-//}
-
 type Volume []map[string]int
-
-//type action string
-
-//func MakeVolume(actionsList []map[action]int) Volume {
-//	return volume{actionsList: actionsList}
-//}
